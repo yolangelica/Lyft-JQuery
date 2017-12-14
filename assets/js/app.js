2f14d72fbe2462aa4d93f42eedd5df1f92b0e84a
yolangelica <rayman.yolanda@gmail.com>
2017-12-14 14:53:36 -0300

    pagina principal

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+		$(".generalCont").hide();
+		$(function(){
+	setTimeout(function() {
+      	$('#splash').fadeOut(500);
+    	$(".generalCont").show();  
+   }, 2000);
+
+		});
+		});
+
+
